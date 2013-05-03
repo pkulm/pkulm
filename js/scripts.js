f7d09150e88a9490a1a09434197647b66f97a0fe
Paige Kulm <paige.kulm@eagles.ewu.edu>
2013-05-02 21:55:30 -0700

    Updated
    added logo, blockquote, fixing nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,6 @@
 // JavaScript Document
 
 $(document).ready(function(){
-	$('.carousel').carousel({
-  interval: 4000,
-  pause: 'hover',
-})
-	$('blockquote').fadeOut(4000).fadeIn(2000);
+	//$('.nav li:first').html('<img src="img/logo.png" />');
 	
 });
